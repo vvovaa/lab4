194e23e371ba3196c261bf13363345267ca758c9
Vladimir <vladimir@Vladimirs-MacBook-Pro.local>
2022-04-06 20:15:20 +0300

    123

---
+++ hello_world/solver_application/CMakeFiles/equation.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vladimir/Documents/TIMP/lab3/lab03/solver_application/equation.cpp" "solver_application/CMakeFiles/equation.dir/equation.cpp.o" "gcc" "solver_application/CMakeFiles/equation.dir/equation.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/vladimir/Documents/TIMP/lab3/lab03/hello_world/formatter_ex_lib/CMakeFiles/formatter_ex.dir/DependInfo.cmake"
+  "/Users/vladimir/Documents/TIMP/lab3/lab03/hello_world/solver_lib/CMakeFiles/solver.dir/DependInfo.cmake"
+  "/Users/vladimir/Documents/TIMP/lab3/lab03/hello_world/formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ solver_lib/solver.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <cmath>
+
+void solve(float a, float b, float c, float& x1, float& x2);
